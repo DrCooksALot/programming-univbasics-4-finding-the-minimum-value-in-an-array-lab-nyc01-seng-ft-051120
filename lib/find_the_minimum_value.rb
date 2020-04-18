21a6b4338dfb2c3fa5e869079d43120865478915
Eli Nass <eliftw@gmail.com>
2020-04-18 17:39:38 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,6 +1,9 @@
 def find_min_value(array)
   min = array.first
-  array.length.times {
-    
+  array.length.times { |index|
+    if array[index] < min
+      min = array[index]
+    end
   }
+  min
 end
